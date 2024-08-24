73e8f6330eb25e1bc358e1f35d678ab67ff485a5
JonatasBSM <jonatasbsmcarvalho@gmail.com>
2024-08-23 21:14:52 -0300

    limpando documentos

--- pages/configuracoes/documentos.vue
+++ pages/configuracoes/documentos.vue
@@ -14,23 +14,38 @@
         <UTabs :items="tabs" >
           <template #item="{ item }">
             <template v-if="item.key == 'tipo-documento'">
-              <crud-table title="Tipo de Documento" modal-component="tipoDocumento" action-class="tipoDocumento" :columns="columnsTipoDocumento"/>
+              <crud-table title="Tipo de Documento" modal-component="tipoDocumento" action-class="tipoDocumento" :columns="[
+                {
+                  label: 'Id',
+                  key: 'id',
+                },
+                {
+                  label: 'Nome',
+                  key: 'st_nome',
+                },
+                {
+                  label: 'Descrição',
+                  key: 'st_descricao',
+                },
+              ]"/>
             </template>
             <template v-else-if="item.key == 'documentacao'">
-              <header class="flex justify-end mt-6">
-                <UButton
-                    icon="i-heroicons-pencil-square"
-                    size="sm"
-                    color="violet"
-                    variant="solid"
-                    label="Nova Documentação"
-                    :trailing="false"
-                    @click="modalDocumentacaoState = true"
-                />
-              </header>
-              <UTable
-                  :columns="columnsDocumentacao"
-              />
+<!--
+              <crud-table title="Documentação" modal-component="documentacao" action-class="documentacao" :columns="[
+                {
+                  label: 'Id',
+                  key: 'id',
+                },
+                {
+                  label: 'Nome',
+                  key: 'st_nome',
+                },
+                {
+                  label: 'Descrição',
+                  key: 'st_descricao',
+                },
+              ]"/>
+-->
             </template>
           </template>
         </UTabs>
@@ -38,22 +53,11 @@
     </div>
   </nuxt-layout>
 
-  <documentacao v-model="modalDocumentacaoState"/>
 </template>
 
 <script setup lang="ts">
 
-import tipoDocumento from "~/components/shared/Modals/tipoDocumento.vue";
-import documentacao from "~/components/shared/Modals/documentacao.vue";
-import type {CreateTipoDocumentoForm} from "~/actions/tipoDocumento/create";
-import type {UpdateTipoDocumentoForm} from "~/actions/tipoDocumento/update";
-import actions from "~/actions";
 import CrudTable from "~/components/shared/tables/crudTable.vue";
-
-const modalTipoDocumentoState = ref(false);
-const modalDocumentacaoState = ref(false);
-
-const selectedTipoDocumento = ref<CreateTipoDocumentoForm|UpdateTipoDocumentoForm|null>(null);
 
 const tabs = [
   {
@@ -66,73 +70,12 @@ const tabs = [
   },
 ];
 
-let tiposDocumentos = ref([]);
-let documentacoes = ref([]);
-
-const columnsTipoDocumento = [
-  {
-    label: 'Id',
-    key: 'id',
-  },
-  {
-    label: 'Nome',
-    key: 'st_nome',
-  },
-  {
-    label: 'Descrição',
-    key: 'st_descricao',
-  },
-];
-
-const columnsDocumentacao = [
-  {
-    label: 'Id',
-    key: 'id',
-  },
-  {
-    label: 'Nome',
-    key: 'st_nome',
-  },
-  {
-    label: 'Descrição',
-    key: 'st_descricao',
-  },
-  {
-    label: 'Ações',
-    key: '',
-  },
-];
-
 const breadcrumbs = [
   { label: 'Configurações', to: '/configuracoes' },
   { label: 'Documentos', to: '/configuracoes/documentos' }
 ]
 
 const device = useDevice();
-
-function open_modal_tipo_documento(tipoDocumento: UpdateTipoDocumentoForm|null = null) {
-
-
-  if (tipoDocumento) {
-    selectedTipoDocumento.value = tipoDocumento;
-  } else {
-    selectedTipoDocumento.value = {
-      st_nome: '',
-      st_descricao: '',
-    };
-  }
-
-  modalTipoDocumentoState.value = true;
-
-}
-
-async function refresh_tipo_documento() {
-  tiposDocumentos.value = (await actions.tipoDocumento.list()).data;
-}
-
-onMounted(async () => {
-    tiposDocumentos.value  = (await actions.tipoDocumento.list()).data;
-});
 
 </script>
 
